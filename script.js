3fcd7a914347895f0b89f1a6521367868573a1c4
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2024-07-25 22:09:32 +0530

    Created The Snake's Head

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX , foodY ;
+//Snake Head position will be Fixed
+let snakeX = 5 , snakeY = 10 ;
 
 const changeFoodPosition = () => {
     // Passing a random 1 - 30 value as Food-Position
@@ -12,6 +14,8 @@ const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     // grid-area is a shorthand property that sets values for grid item's start and end lines for both the row and column
     
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+
     // Creating a food-div and inserting it in the playboard element
     playBoard.innerHTML = htmlMarkup ;
 }
